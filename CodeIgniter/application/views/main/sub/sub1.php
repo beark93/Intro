ff7dca7c5184346c504434569909452a4b8a2da7
beark93 <beark93@gmail.com>
2019-06-03 11:50:34 +0900

    change home main image

--- CodeIgniter/application/views/main/sub/sub1.php
+++ CodeIgniter/application/views/main/sub/sub1.php
@@ -1,10 +1,10 @@
 <div class='contents-sub contents-sub1'>
   <div style="width:100%; position:relative; float:left;">
-    <div style="width: 250px; height: 250px; margin:0 auto;">
-      <img src="/image/me.jpg" style="width:100%; height:100%; object-fit: cover; object-position: top; border-radius: 50%;">
+    <div style="width: 400px; height: 280px; margin:0 auto;">
+      <img src="/image/main.jpg" style="width:100%; height:100%; object-fit: cover; object-position: 0 -75px;">
     </div>
   </div>
-    <div style="width:100%; position:relative; float:left; margin-top:60px; text-align:center;">
+    <div style="width:100%; position:relative; float:left; margin-top:35px; text-align:center;">
       <span class="home-font">포털사이트 이름검색 시 자신이 제일 먼저 나오는 것을 꿈구며</span></br>
       <span class="home-font">Introduce와 Portfolio 탭의 스크롤이 짧아지기 위해 노력하는</span></br>
       <span class="home-font">웹 개발자 김병영의 포트폴리오 페이지 입니다.</span>
